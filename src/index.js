cf693a74207c95873a990f31f222e6036427ff90
Artem Shadrin <explay97@gmail.com>
2024-03-30 00:22:02 +0500

    last sect

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-import './main.scss';
-function component(text) {
-  const element = document.createElement('h1');
-  element.textContent = text;
-  return element;
-}
-
-document.body.prepend(component('Проект собран на Webpack'));
+import './scss/index.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'src', 'template.html'),
+            template: path.join(__dirname, 'src', 'index.html'),
             filename: 'index.html',
         }),
         new FileManagerPlugin({
