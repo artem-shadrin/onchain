64ba3617920b1027b1c735aaa9f0b7c6ba7e8c7f
Artem Shadrin <explay97@gmail.com>
2024-03-30 20:55:08 +0500

    fix burger

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './scss/index.scss';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const burgerBtnEl = document.getElementById('burger-button')
+    const burgerMenuEl = document.getElementById('burger-menu')
+    burgerBtnEl.addEventListener('click', () => {
+        burgerBtnEl.classList.toggle('active')
+        burgerMenuEl.classList.toggle('active')
+        document.body.classList.toggle('lock')
+    })
+})
